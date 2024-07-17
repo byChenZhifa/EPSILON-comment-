2cb2393c9f8fad089bce8799bbee54fdb495142e
Zhifa Chen <chenzf2013@163.com>
2024-07-17 19:40:25 +0800

    spline.h注释代码

--- core/common/inc/common/spline/spline.h
+++ core/common/inc/common/spline/spline.h
@@ -9,6 +9,7 @@
 namespace common {
 
 template <int N_DEG, int N_DIM>
+// 定义了一个通用的样条（Spline）类模板，用于在给定的参数化域上创建和评估多维多项式样条
 class Spline {
  public:
   typedef PolynomialND<N_DEG, N_DIM> PolynomialType;
@@ -16,8 +17,8 @@ class Spline {
   Spline() {}
 
   /**
-   * @brief Set the vector domain
-   * @param vec_domain: input vec domain
+   * @brief 设置参数化域
+   * @param vec_domain 输入的参数化域向量
    */
   void set_vec_domain(const std::vector<decimal_t>& vec_domain) {
     assert(vec_domain.size() > 1);
@@ -26,19 +27,19 @@ class Spline {
   }
 
   /**
-   * @brief Get the number of segments of the spline
-   * @param n number of the segments
+   * @brief Get the number of segments of the spline 获取样条的段数
+   * @param n number of the segments  段数
    */
   int num_segments() const { return static_cast<int>(poly_.size()); }
 
   /**
-   * @brief Get the vec domain of the spline
-   * @param n number of the segments
+   * @brief Get the vec domain of the spline 获取样条的参数化域向量
+   * @param n number of the segments   参数化域向量
    */
   std::vector<decimal_t> vec_domain() const { return vec_domain_; }
 
   /**
-   * @brief Get the begin of the parameterization
+   * @brief Get the begin of the parameterization 获取参数化的起始值
    */
   decimal_t begin() const {
     if (vec_domain_.size() < 1) return 0.0;
@@ -46,7 +47,7 @@ class Spline {
   }
 
   /**
-   * @brief Get the end of the parameterization
+   * @brief Get the end of the parameterization 获取参数化的结束值
    */
   decimal_t end() const {
     if (vec_domain_.size() < 1) return 0.0;
@@ -57,6 +58,12 @@ class Spline {
    * @brief Return a reference to a certain 1D polynomial
    * @param n index of the segment
    * @param j index of the dimension
+   */
+   /**
+   * @brief 返回对某个1D多项式的引用
+   * @param n 段索引
+   * @param j 维度索引
+   * @return 对应的多项式
    */
   Polynomial<N_DEG>& operator()(int n, int j) {
     assert(n < this->num_segments());
@@ -69,6 +76,13 @@ class Spline {
    * @param s parameterization, s should be in the vector domain
    * @param d derivate to take
    * @note  the function will automatically do extrapolation if out of domain
+   */
+  /**
+   * @brief 在给定参数化值下评估样条
+   * @param s 参数化值，s 应在参数化域内
+   * @param d 求导次数
+   * @param ret 返回值
+   * @return 错误类型
    */
   ErrorType evaluate(const decimal_t s, int d, Vecf<N_DIM>* ret) const {
     int num_pts = vec_domain_.size();
@@ -91,6 +105,12 @@ class Spline {
     return kSuccess;
   }
 
+  /**
+   * @brief 在给定参数化值下评估样条
+   * @param s 参数化值，s 应在参数化域内
+   * @param ret 返回值
+   * @return 错误类型
+   */
   ErrorType evaluate(const decimal_t s, Vecf<N_DIM>* ret) const {
     int num_pts = vec_domain_.size();
     if (num_pts < 1) return kIllegalInput;
@@ -112,6 +132,9 @@ class Spline {
     return kSuccess;
   }
 
+  /**
+   * @brief 打印样条信息
+   */
   void print() const {
     int num_polys = static_cast<int>(poly_.size());
     for (int i = 0; i < num_polys; i++) {
@@ -130,8 +153,8 @@ class Spline {
   }
 
  private:
-  vec_E<PolynomialType> poly_;
-  std::vector<decimal_t> vec_domain_;
+  vec_E<PolynomialType> poly_;// 多维多项式的向量
+  std::vector<decimal_t> vec_domain_;// 参数化域向量
 };
 
 }  // namespace common
